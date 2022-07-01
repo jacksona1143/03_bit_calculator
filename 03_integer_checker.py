e38bd0b0297695f9d2af669efae5343990b47424
jacksona1143 <101374259+jacksona1143@users.noreply.github.com>
2022-07-01 14:52:30 +1200

    Update 03_integer_checker.py

--- 03_integer_checker.py
+++ 03_integer_checker.py
@@ -1,3 +1,5 @@
+# functions go here
+
 # checks input is  a number more than a given value
 def num_check (question):
     valid = False
@@ -6,14 +8,18 @@ def num_check (question):
         error = "Please enter a value more than 0"
 
         try:
+
+            # asks user to enter a question
             response = float(input(question))
 
+            # checks number is more than zero
             if response > 0:
                 return response
 
+            # outputs error if input is invalid
             else:
                 print(error)
-                
+                print()
 
         except ValueError:
                 print("Please enter a number")
